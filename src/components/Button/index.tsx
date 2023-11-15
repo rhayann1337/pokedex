c06dc30039d324d299ee807fb5d92c989a8d3c90
Kelvi <kelvingremista2015@hotmail.com>
2023-11-14 23:42:01 -0300

    :sparkles: feat: added routes and some logic page NotFound

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,21 @@
-import * as S from './styles'
+import { ButtonHTMLAttributes } from "react";
+import * as S from "./styles";
 
-interface IButtonComponent {
-    color: string
-    text: string
-    whidth: number
-}
-export function ButtonComponent({ color, text, whidth }: IButtonComponent) {
-    return <S.Button whidth={whidth} color={color}>{text}</S.Button>
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    color: string;
+    text: string;
+    width: number;
+};
+
+export function ButtonComponent({
+  color,
+  text,
+  width,
+  ...props
+}: ButtonProps) {
+  return (
+    <S.Button  {...props} width={width} color={color}>
+      {text}
+    </S.Button>
+  );
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 interface IButton {
     color: string;
-    whidth: number;
+    width: number;
 }
 
 export const Button = styled.button<IButton>`
 border: none;
 border-radius: 11px;
 background-color: ${({ color }) => color};
-width: ${({ whidth }) => `${whidth}rem`};
+width: ${({ width }) => `${width}rem`};
 height: 3rem;
 cursor: pointer;
 box-shadow: 0px - 9px 0px 0px rgba(0, 0, 0, 0.18) inset;
